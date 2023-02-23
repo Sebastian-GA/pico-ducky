3092cc4a293a4703cc601af630f035ef74847db9
Sebastian-GA <isebastian.ga@gmail.com>
2023-02-23 17:55:56 -0500

    don't show usb drive by default

--- boot.py
+++ boot.py
@@ -8,7 +8,7 @@ import board
 import digitalio
 import storage
 
-noStorage = False
+noStorageStatus = False
 noStoragePin = digitalio.DigitalInOut(GP15)
 noStoragePin.switch_to_input(pull=digitalio.Pull.UP)
 noStorageStatus = noStoragePin.value
@@ -16,23 +16,7 @@ noStorageStatus = noStoragePin.value
 # If GP15 is not connected, it will default to being pulled high (True)
 # If GP is connected to GND, it will be low (False)
 
-# Pico:
-#   GP15 not connected == USB visible
-#   GP15 connected to GND == USB not visible
-
-# Pico W:
-#   GP15 not connected == USB NOT visible
-#   GP15 connected to GND == USB visible
-
-if(board.board_id == 'raspberry_pi_pico'):
-    # On Pi Pico, default to USB visible
-    noStorage = not noStorageStatus
-elif(board.board_id == 'raspberry_pi_pico_w'):
-    # on Pi Pico W, default to USB hidden by default
-    # so webapp can access storage
-    noStorage = noStorageStatus
-
-if(noStorage == True):
+if (noStorageStatus == True):
     # don't show USB drive to host PC
     storage.disable_usb_drive()
     print("Disabling USB drive")
